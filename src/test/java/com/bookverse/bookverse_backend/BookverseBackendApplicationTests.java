994f5a7485573205bdc1e0e8ef1caba80247b670
Ullas Rao Pawar <ullaspawarrao@gmail.com>
2026-07-28 22:26:55 +0530

    Initial Spring Boot project setup with PostgreSQL and Hello API

---
+++ src/test/java/com/bookverse/bookverse_backend/BookverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookverse.bookverse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
